19949bfef15c558d6578c397c303d4253ed4adb6
Setsuna F. Seiei <takraw_s@hotmail.com>
2020-09-16 16:27:01 +0700

    update

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -6,6 +6,17 @@
         
     </v-toolbar>
     <v-card-text>
+    <form @submit.prevent="authentication">
+        
+        <v-text-field label="username" type="text" dense outlined v-model="username"></v-text-field>
+        <v-text-field label="password" type="password" dense outlined v-model="password"></v-text-field>
+
+        <v-btn color="info" @click="authentication" justify-center type="submit">LOGIN</v-btn>
+
+    </form>
+    </v-card-text>
+
+    <!-- <v-card-text>
         <v-text-field label="username" type="text" dense outlined v-model="username"></v-text-field>
         <v-text-field label="password" type="password" dense outlined v-model="password"></v-text-field>
         
@@ -14,7 +25,7 @@
     <v-card-actions>
         <v-spacer></v-spacer>
         <v-btn color="info" @click="authentication" justify-center>LOGIN</v-btn>
-    </v-card-actions>
+    </v-card-actions> -->
 
 </v-card>
 </template>
